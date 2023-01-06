14229ff034a97d39c51badf8e03f3fb17932eff2
Voodoo30fps <voodoo30fps@gmail.com>
2023-01-06 10:53:36 +0200

    route with dummy data in web routes

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,29 @@
+<h1>{{ $heading }}</h1>
+
+@if(count($listings)==0)
+<p>No listings found!</p>
+
+@else
+@foreach ($listings as $listing)
+<h2>
+    {{ $listing['title'] }}
+</h2>
+<p>
+    {{ $listing['description'] }}
+</p>
+@endforeach
+@endif
+
+{{-- @unless (count($listings)==0)
+@foreach ($listings as $listing)
+<h2>
+    {{ $listing['title'] }}
+</h2>
+<p>
+    {{ $listing['description'] }}
+</p>
+@endforeach
+
+@else
+<p>No listings found!</p>
+@endunless --}}
--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non velit et nulla volutpat vestibulum. Suspendisse vel placerat ipsum, in congue elit. Aenean sem nunc, suscipit sed elementum a, vulputate eu justo. Duis hendrerit sagittis lorem eu tincidunt. Nunc ac suscipit ante. Suspendisse et mauris sit amet leo porta pulvinar nec id mauris. Nam vestibulum sollicitudin arcu, posuere dignissim odio faucibus vitae. Phasellus blandit est at risus varius commodo. In hac habitasse platea dictumst. Curabitur augue orci, laoreet eu cursus ac, sodales ultricies ipsum. Donec sit amet consectetur risus. In volutpat enim ut neque tempor, sed blandit mi suscipit. Duis et ante mattis urna imperdiet tristique.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non velit et nulla volutpat vestibulum. Suspendisse vel placerat ipsum, in congue elit. Aenean sem nunc, suscipit sed elementum a, vulputate eu justo. Duis hendrerit sagittis lorem eu tincidunt. Nunc ac suscipit ante. Suspendisse et mauris sit amet leo porta pulvinar nec id mauris. Nam vestibulum sollicitudin arcu, posuere dignissim odio faucibus vitae. Phasellus blandit est at risus varius commodo. In hac habitasse platea dictumst. Curabitur augue orci, laoreet eu cursus ac, sodales ultricies ipsum. Donec sit amet consectetur risus. In volutpat enim ut neque tempor, sed blandit mi suscipit. Duis et ante mattis urna imperdiet tristique.'
+            ]
+        ]
+    ]);
 });
 
-Route::get('/hello', function () {
-    return response('<h1>Hello world!</h1>', 200)
-        ->header('Content-Type', 'text/plain')
-        ->header('foo', 'bar');
-});
+// Route::get('/hello', function () {
+//     return response('<h1>Hello world!</h1>', 200)
+//         ->header('Content-Type', 'text/plain')
+//         ->header('foo', 'bar');
+// });
 
-Route::get('/posts/{id}', function ($id) {
-    // ddd($id);
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
-// ^ where = constraint with reg expression / helper function
+// Route::get('/posts/{id}', function ($id) {
+//     // ddd($id);
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+// // ^ where = constraint with reg expression / helper function
 
-Route::get('/search', function (Request $request) {
-    return $request->name . ' ' . $request->city;
-});
+// Route::get('/search', function (Request $request) {
+//     return $request->name . ' ' . $request->city;
+// });
