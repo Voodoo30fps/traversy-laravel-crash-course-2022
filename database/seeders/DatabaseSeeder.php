360e7dc5d4dec59528a65f2dcd625f300b4ad69b
Voodoo30fps <voodoo30fps@gmail.com>
2023-01-06 11:52:09 +0200

    listings table migration + dummy factory seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
@@ -22,3 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
     }
 }
+
+// HOW TO RUN THE SEEDER
+// php artisan db:seed
