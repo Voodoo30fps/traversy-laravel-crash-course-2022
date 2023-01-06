220a3b992b1212a78668dda59bd9bcf7c988a94a
Voodoo30fps <voodoo30fps@gmail.com>
2023-01-06 10:42:00 +0200

    dummy routes (web + api)

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts', function () {
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post One'
+            ]
+        ]
+    ]);
+});
+// api routes must be accessed with the 'api' prefix eg. /api/posts
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
