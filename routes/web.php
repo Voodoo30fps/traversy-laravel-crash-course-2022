64ad11d615d98d8a97921bea65c3753a0edde114
Voodoo30fps <voodoo30fps@gmail.com>
2023-01-09 17:08:34 +0200

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-// use Illuminate\Http\Request;
-
-use App\Http\Controllers\ListingController;
-use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,3 +44,9 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
