385d107df54acf47bc47dd0bbdd151f34ce2358b
Voodoo30fps <voodoo30fps@gmail.com>
2023-01-09 09:04:49 +0200

    store listing, validation, mass assignment rule

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 // use Illuminate\Http\Request;
 
 use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,9 @@ Route::get('/', [ListingController::class, 'index']);
 
 // Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
